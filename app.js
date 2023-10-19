a4b0a434ddb9eafb8e170505e9b4f944072ec572
heyu-lim <heyulim99@gmail.com>
2023-10-20 01:11:26 +0800

    clean-up and adjustments

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
+const reservationForm = document.querySelector('.reservation-form');
+const nameInput = document.getElementById('name');
+const contactInput = document.getElementById('contact');
+const dateInput = document.getElementById('date');
+const timeInput = document.getElementById('time');
+const paxInput = document.getElementById('pax');
+
+
+reservationForm.addEventListener('submit', function (event) {
+    if (nameInput.value === '' || contactInput.value === '' || dateInput.value === '' || timeInput.value === '' || paxInput.value === '') {
+        event.preventDefault(); 
+        alert('Please fill out all fields.'); 
+    }
+});
 
